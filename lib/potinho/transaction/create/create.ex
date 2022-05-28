b57e86d21c87d88182dab6151bb36b7684b11423
romulogarofalo <romulogarofalo@gmail.com>
2022-05-28 03:22:54 -0300

    :white_check_mark: add test and fixed bugs of create transaction and chargeback

--- lib/potinho/transaction/create/create.ex
+++ lib/potinho/transaction/create/create.ex
@@ -9,9 +9,9 @@ defmodule Potinho.Transaction.Create do
   def run(%{cpf_reciever: cpf_reciever, amount: amount, id_sender: id_sender}) do
     Multi.new()
     |> Multi.run(:get_sender_step, get_sender(id_sender))
-    |> Multi.run(:get_reciever_step, get_reciever(cpf_reciever))
     |> Multi.run(:verify_balances_step, verify_balances(amount))
     |> Multi.run(:subtract_from_sender_step, &subtract_from_sender/2)
+    |> Multi.run(:get_reciever_step, get_reciever(cpf_reciever))
     |> Multi.run(:add_to_reciever_step, &add_to_reciever/2)
     |> Multi.run(:create_transaction_register, &create_transaction/2)
     |> Repo.transaction()
@@ -44,27 +44,28 @@ defmodule Potinho.Transaction.Create do
   end
 
   defp verify_balances(transfer_amount) do
-    fn _repo, %{get_sender_step: user_sender, get_reciever_step: user_reciever} ->
+    fn _repo, %{get_sender_step: user_sender} ->
       if user_sender.balance.amount < transfer_amount.amount,
         do: {:error, :balance_too_low},
-        else: {:ok, {user_sender, user_reciever, transfer_amount.amount}}
+        else: {:ok, {user_sender, transfer_amount.amount}}
     end
   end
 
-  defp subtract_from_sender(repo, %{verify_balances_step: {user_sender, _, verified_amount}}) do
+  defp subtract_from_sender(repo, %{verify_balances_step: {user_sender, verified_amount}}) do
     user_sender
     |> User.changeset(%{balance: Money.subtract(user_sender.balance, verified_amount)})
     |> repo.update()
   end
 
-  defp add_to_reciever(repo, %{verify_balances_step: {_, user_reciever, verified_amount}}) do
+  defp add_to_reciever(repo, %{verify_balances_step: {_, verified_amount}, get_reciever_step: user_reciever}) do
     user_reciever
     |> User.changeset(%{balance: Money.add(user_reciever.balance, verified_amount)})
     |> repo.update()
   end
 
   defp create_transaction(repo, %{
-         verify_balances_step: {user_sender, user_reciever, verified_amount}
+         verify_balances_step: {user_sender, verified_amount},
+         get_reciever_step: user_reciever
        }) do
     Transaction.create_changeset(%{
       user_sender_id: user_sender.id,
--- lib/potinho_web/controllers/transaction_controller.ex
+++ lib/potinho_web/controllers/transaction_controller.ex
@@ -1,11 +1,15 @@
 defmodule PotinhoWeb.TransactionController do
   use PotinhoWeb, :controller
 
+  require Logger
+
   alias Potinho.Transaction.Create.Input, as: CreateInput
   alias Potinho.Transaction.Create
   alias Potinho.Transaction.Chargeback
   alias Potinho.Transaction.Index.Input, as: IndexInput
   alias Potinho.Transaction.Index
+  alias PotinhoWeb.Helpers.ErrorHandler
+  alias Potinho.Guardian
 
   def create(conn, params) do
     with %{"id" => user_sender_id} <- Guardian.Plug.current_resource(conn),
@@ -15,6 +19,16 @@ defmodule PotinhoWeb.TransactionController do
       conn
       |> put_status(:created)
       |> render("created.json", transaction)
+    else
+      {:error, :verify_balances_step, :balance_too_low, _} ->
+        ErrorHandler.bad_request(conn, "balance too low")
+      {:error, :get_reciever_step, :user_not_found, _} ->
+        ErrorHandler.not_found(conn, "cpf not found")
+      {:error, :tabela_ta_em_lock} ->
+        ErrorHandler.internal_server_error(conn)
+      error ->
+        Logger.error("#{__MODULE__}.create error=#{error}")
+        ErrorHandler.internal_server_error(conn)
     end
   end
 
@@ -24,11 +38,13 @@ defmodule PotinhoWeb.TransactionController do
          {:ok, %{user_id: user_id, initial_date: initial_date, end_date: end_date}} <-
            IndexInput.validate(new_map),
          {:ok, transactions} <- Index.run(user_id, initial_date, end_date) do
-      IO.inspect(transactions)
 
       conn
       |> put_status(:ok)
       |> render("index.json", transactions)
+    else
+      {:error, message} ->
+        IO.inspect(message, label: "error?")
     end
   end
 
@@ -38,6 +54,14 @@ defmodule PotinhoWeb.TransactionController do
       conn
       |> put_status(:no_content)
       |> render("chargeback.json", transaction)
+    else
+      {:error, :verify_balance_step, :balance_too_low, _} ->
+        ErrorHandler.bad_request(conn, "chargeback is not possible")
+      {:error, :get_transaction_step, _, _} ->
+        ErrorHandler.not_found(conn, "transaction not found")
+      error ->
+        Logger.error("#{__MODULE__}.chargeback error=#{error}")
+        ErrorHandler.internal_server_error(conn)
     end
   end
 end
